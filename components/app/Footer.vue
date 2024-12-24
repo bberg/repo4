53c7c20835f3db195d26aa4219e42785e45921be
Ben Berg <benberg00@gmail.com>
2024-12-24 15:37:58 -0500

    robots update and removed h2 in footer

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -23,7 +23,6 @@ const navigation = [
 
 <template>
   <footer class="border-t dark:border-gray-600 font-display" aria-labelledby="footer-heading">
-    <h2 id="footer-heading" class="sr-only">Footer</h2>
     <div class="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
       <div class="xl:grid xl:grid-cols-3 xl:gap-8">
         <div class="space-y-8">
